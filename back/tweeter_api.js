f89b1c7a14d3e8851d2b0bb6a45bd7595c7f7402
Loic Chau <loic.chau@student-cs.fr>
2019-12-04 18:19:48 +0100

    twitter_api start

---
+++ back/tweeter_api.js
@@ -0,0 +1,21 @@
+const Twit = require('twit');
+const config = require('./config');
+
+const T  = new Twit({
+    consumer_key: config.twitterCred.consumer_key,
+    consumer_secret: config.twitterCred.consumer_private,
+    access_token: config.twitterCred.access_token,
+    access_token_secret: config.twitterCred.access_token_private
+});
+
+/*T.get('followers/ids', { screen_name: 'tolga_tezel' },  function (err, data, response) {
+    console.log(data)
+  })*/
+
+T.get('account/verify_credentials', { skip_status: true })
+  .catch(function (err) {
+    console.log('caught error', err.stack)
+  })
+  .then(function () {
+    console.log('Login to Twitter successful !')
+  });
