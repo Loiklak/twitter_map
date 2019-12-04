b0ef337123035a7aead2e5ea70690c137832516b
Loic Chau <loic.chau@student-cs.fr>
2019-12-04 23:58:33 +0100

    fixed twitter search and added comments

--- back/tweeter_api.js
+++ back/tweeter_api.js
@@ -20,11 +20,20 @@ function check_connection () {
 };
 
 
-function search (hashtag, last_tweet_id) {
-    //Permet de récupérer les tweets à partir du last_tweet_id (le dernier tweet en base de donnée typiquement)
+function search (hashtag, last_tweet_id=0) {
+    /*Permet de récupérer les tweets associé au hashtag.
+    [OPTIONAL] Tweets seulement après le last_tweet_id (le dernier tweet en base de donnée typiquement)
+    Renvoie un array avec dedans les JSON de chaque tweet
+    Les infos utiles sont : (où tweet est un object de data.statuses)
+    * tweet.id (pour savoir quel est le dernier tweet que l'on a reçu)
+    * tweet.statuses.created_at (la date)
+    * tweet.statuses.user.location (pour la localisation du tweet)
+    */
+
     T.get('search/tweets', { q: `#${hashtag}`, count: 100, since_id: last_tweet_id}, function(err, data, response) {
-        console.log(data)
+        //console.log(data.statuses.map(tweet => tweet.user.location));
+        return data.statuses
       })
 }
 
-//search("chaserice", 1202145)
+search("chaserice", 1202145)
