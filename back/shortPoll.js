66ceff2c7357e2f8f8753899d90a9c4805d99991
Loic Chau <loic.chau@student-cs.fr>
2019-12-05 16:16:20 +0100

    added shortpolling function

---
+++ back/shortPoll.js
@@ -0,0 +1,63 @@
+const db = require("./database");
+const tweeter = require("./tweeter_api");
+
+function shortPoll(hashtag, lastTweetId=0) {
+    tweeter.search(hashtag, lastTweetId, scrapAndInsert);
+
+    function scrapAndInsert(err, data, response) {
+        const formated = (data.statuses.map(function (tweet) {
+            return {
+                "id": tweet.id,
+                "date": toMySQLDate(tweet.created_at),
+                "location": tweet.user.location
+            }
+        }))
+
+        formated.map((tweet) => {tweet.location == '' ? void(0) :
+        db.Tweet.create({
+            tweetId: tweet.id,
+            date: tweet.date,
+            location: tweet.location,
+            hashtag: hashtag
+        })
+        .then((results) => console.log(results.id))
+    })
+    }
+
+    function toMySQLDate(twitDate) {
+        const array = twitDate.split(" ");
+        return (array[5] + "-" + toMonthNumber(array[1]) + "-" + array[2] + " " + array[3]);
+    };
+
+    function toMonthNumber (month) {
+        switch (month) {
+            case "Jan":
+                return 01;
+            case "Feb":
+                return 02;
+            case "Mar":
+                return 03;
+            case "Apr":
+                return 04;
+            case "May":
+                return 05;
+            case "Jun":
+                return 06;
+            case "Jul":
+                return 07;
+            case "Aug":
+                return 08;
+            case "Sep":
+                return 09;
+            case "Oct":
+                return 10;
+            case "Nov":
+                return 11;
+            case "Dec":
+                return 12;
+        }
+    }
+}
+
+shortPoll("DonaldTrump")
+shortPoll("greve")
