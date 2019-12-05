7df331a53adb88026a70af36260412cb93b225ec
Loic Chau <loic.chau@student-cs.fr>
2019-12-05 16:06:36 +0100

    cleaned tweeter_api

--- back/tweeter_api.js
+++ back/tweeter_api.js
@@ -19,52 +19,7 @@ function check_connection () {
     });
 };
 
-
-function scrapping(err, data, response) {
-    function toMySQLDate(twitDate) {
-        function toMonthNumber (month) {
-            switch (month) {
-                case "Jan":
-                    return 01;
-                case "Feb":
-                    return 02;
-                case "Mar":
-                    return 03;
-                case "Apr":
-                    return 04;
-                case "May":
-                    return 05;
-                case "Jun":
-                    return 06;
-                case "Jul":
-                    return 07;
-                case "Aug":
-                    return 08;
-                case "Sep":
-                    return 09;
-                case "Oct":
-                    return 10;
-                case "Nov":
-                    return 11;
-                case "Dec":
-                    return 12;
-            }
-        }
-        const array = twitDate.split(" ");
-        return (array[5] + "-" + toMonthNumber(array[1]) + "-" + array[2] + " " + array[3]);
-    };
-    data.statuses.map(function (tweet) {
-        console.log({
-            "id": tweet.id,
-            "date": toMySQLDate(tweet.created_at),
-            "location": tweet.user.location
-        })
-    })
-}
-
-
-function search (hashtag, last_tweet_id=0, callback=scrapping) {
-    /*Permet de récupérer les tweets associé au hashtag.
+/*Permet de récupérer les tweets associé au hashtag.
     [Arg] Callback : Mettre dedans la fonction qui va mettre les tweets dans la BDD
     [OPTIONAL] Tweets seulement après le last_tweet_id (le dernier tweet en base de donnée typiquement)
     Renvoie un array avec dedans les JSON de chaque tweet
@@ -73,8 +28,8 @@ function search (hashtag, last_tweet_id=0, callback=scrapping) {
     * tweet.statuses.created_at (la date)
     * tweet.statuses.user.location (pour la localisation du tweet)
     */
-
+function search (hashtag, last_tweet_id=0, callback=(err, data, response)=>console.log(data)) {
     T.get('search/tweets', { q: `#${hashtag}`, count: 100, since_id: last_tweet_id}, callback)
 }
 
-search("chaserice", 120214)
+module.exports = { search };
